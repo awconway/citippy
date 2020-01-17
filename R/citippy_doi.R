6c0b653dd885c353880ba8e0a464daae09ab5767
Aaron Conway <aaronconwayphd@gmail.com>
2020-01-17 10:43:26 -0500

    doi function added

---
+++ R/citippy_doi.R
@@ -0,0 +1,38 @@
+#' citippy_doi
+#' 
+#' @rdname citippy_doi
+#' @name citippy doi
+#' 
+#' @param doi doi string
+#' 
+#' @export
+#' @examples 
+#' 
+#' citippy_doi("10.1136/heartjnl-2013-305289")
+
+
+citippy_doi <- function(doi){
+  
+  successes <- logical(length(doi))
+  for (i in seq_along(doi)){
+    temp <- httr::GET(httr::modify_url('https://doi.org/', path = doi[i]),
+                      config = list(followlocation = TRUE),
+                      httr::add_headers(Accept = "application/x-bibtex"))
+    if (!httr::http_error(temp)){
+      clipr::write_clip(httr::content(temp, as = "text", encoding = "UTF-8"))
+      cat(httr::content(temp, as = "text", encoding = "UTF-8"))
+      message("The bibtex entry above has been copied to your clipboard")
+      successes[i] <- TRUE
+      ## if (is.raw(temp))
+      ##   temp <- rawToChar(temp)
+    }
+  }
+  if (!all(successes)){
+    failures <- paste(sQuote(doi[!successes]), collapse = ", ")
+    message(gettextf("unable to retrieve bibliographic data for %s%s",
+                     "the following supplied DOIs: ",
+                     failures))
+  }
+}
+
+citippy_doi("10.1136/heartjnl-2013-305289")
