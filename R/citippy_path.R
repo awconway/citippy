34810b88e8cb0c70d49621afb7d03af6ca93383e
Aaron Conway <aaronconwayphd@gmail.com>
2020-01-30 14:45:34 -0500

    citippy_path added

---
+++ R/citippy_path.R
@@ -0,0 +1,15 @@
+#' add citation to rmarkdown document that will show as tooltip on hover
+#' 
+#' @name citippy_path
+#' @rdname citippy_path
+#' 
+#' @param ref_path Path to your .bib file with bibtex entries
+#' 
+#' @export
+#' 
+citippy_path <- function(ref_path){
+  bibdf <<- bib2df::bib2df(ref_path, separate_names = TRUE)
+  bibref <<- RefManageR::ReadBib(ref_path)
+}
+#' @example 
+#' citippy_path(ref_path = "references.bib")
